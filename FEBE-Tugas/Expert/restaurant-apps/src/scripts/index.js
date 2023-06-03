58d64fbde7daf42e438afa67cc239fa6602b4435
Widha Astianna <widhaastianna10@gmail.com>
2023-06-03 22:25:48 +0700

    Upload progress praktik

---
+++ FEBE-Tugas/Expert/restaurant-apps/src/scripts/index.js
@@ -0,0 +1,6 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+import '../component/app-bar.js';
+
+console.log('Hello Coders! :)');
